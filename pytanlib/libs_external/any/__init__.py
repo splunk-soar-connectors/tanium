c33e1ee55c0ba6f44f645702371663a7b1e20f92
splunk-soar-connectors-admin <admin@splunksoar>
2021-12-02 09:34:00 -0800

    De-referencing submodules

---
+++ pytanlib/libs_external/any/__init__.py
@@ -0,0 +1,13 @@
+"""Python packages for any operating system.
+
+Setup commands::
+
+    # change directory to 'any'
+    # remove everything in 'any' except this __init__.py
+    pip install 'requests[security]' --target .
+    pip install xmltodict --target .
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
